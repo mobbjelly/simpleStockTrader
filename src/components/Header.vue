9371e118ccef8df319ae778f93a1090550b0936a
Jeremy Chen <clipclop182@gmail.com>
2017-04-26 17:15:41 +0800

    complete fetching data from firebase

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -47,9 +47,10 @@
       }
     },
     methods: {
-      ...mapActions([
-        'randomizeStocks'
-      ]),
+      ...mapActions({
+        randomizeStocks : 'randomizeStocks',
+        fetchData: 'loadData'
+      }),
       endDay() {
         this.randomizeStocks()
       },
@@ -63,7 +64,7 @@
         this.$http.put('data.json', data)
       },
       loadData() {
-        
+        this.fetchData()
       }
     }
   }
