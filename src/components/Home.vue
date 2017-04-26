9994003b9aab1d1cc4a985e9d0c547c832a7399d
Jeremy Chen <clipclop182@gmail.com>
2017-04-26 14:01:33 +0800

    display the funds

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,3 +1,16 @@
 <template>
-  <h1>Home Component</h1>
+  <div>
+    <h1>Trade or View your portfolio</h1>
+    <p>Your funds: {{ funds }}</p>
+  </div>
 </template>
+
+<script>
+  export default {
+    computed: {
+      funds () {
+        return this.$store.getters.funds
+      }
+    }
+  }
+</script>
